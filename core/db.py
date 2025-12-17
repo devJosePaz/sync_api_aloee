d74ff09be7ef928b043d1fd8b6e205f26a691fe1
Jose Gomes da Paz <jose.paz@AD.FORMIL.COM>
2025-12-17 10:10:32 -0300

    refact: ajustando o db para rodar via terminal e .exe.

--- core/db.py
+++ core/db.py
@@ -1,4 +1,3 @@
-# core/db.py
 import pyodbc
 from core.config import settings
 
@@ -7,15 +6,12 @@ def get_connection():
     Retorna conexão pyodbc (autocommit False).
     """
     conn_str = (
-    f"DRIVER={{{settings.MSSQL_DRIVER}}};"
-    f"SERVER={settings.MSSQL_SERVER};"
-    f"DATABASE={settings.MSSQL_DATABASE};"
-    f"UID={settings.MSSQL_USER};"
-    f"PWD={settings.MSSQL_PASSWORD};"
-    "TrustServerCertificate=yes;"
-    "Network=DBMSSOCN;"
+        f"DRIVER={{{settings.MSSQL_DRIVER}}};"
+        f"SERVER={settings.MSSQL_SERVER};"
+        f"DATABASE={settings.MSSQL_DATABASE};"
+        f"UID={settings.MSSQL_USER};"
+        f"PWD={settings.MSSQL_PASSWORD};"
+        "TrustServerCertificate=yes;"
+        "Network=DBMSSOCN;"  # força TCP/IP
     )
-
-
-
     return pyodbc.connect(conn_str, autocommit=False)
